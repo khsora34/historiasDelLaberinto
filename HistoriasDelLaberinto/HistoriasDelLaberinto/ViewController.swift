7fbb8bdd7136ddfae7a5a6d9164dc13de547cbb0
Francisco del Real Escudero <francisco.real@ciberexperis.es>
2019-02-20 17:00:21 +0100

    First commit.

---
+++ HistoriasDelLaberinto/HistoriasDelLaberinto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HistoriasDelLaberinto
+//
+//  Created by SYS_CIBER_ADMIN on 20/02/2019.
+//  Copyright © 2019 SetonciOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
