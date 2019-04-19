44f8d85132cbeaed8c81d9b15a00bdae7ec9d9ef
Francisco del Real Escudero <francisco.real@ciberexperis.es>
2019-04-19 18:07:36 +0200

    Added dependencies, a module provider and a router provider.

---
+++ HistoriasDelLaberinto/HistoriasDelLaberinto/iOS/dependencies/RouterProvider.swift
@@ -0,0 +1,13 @@
+class RouterProvider {
+    let moduleProvider: ModuleProvider
+    let drawer: MainViewController
+    
+    init(drawer: MainViewController, moduleProvider: ModuleProvider) {
+        self.drawer = drawer
+        self.moduleProvider = moduleProvider
+    }
+    
+    var exampleSceneRouter: ExampleSceneRoutingLogic {
+        return ExampleSceneRouter(moduleProvider: moduleProvider, drawer: drawer)
+    }
+}
