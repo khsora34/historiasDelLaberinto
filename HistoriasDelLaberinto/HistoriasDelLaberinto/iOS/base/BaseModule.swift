9a37f776308480c6543d98c79794f39616aaf646
Francisco del Real Escudero <francisco.real@ciberexperis.es>
2019-04-19 17:59:10 +0200

    Added base modules with functionality.

---
+++ HistoriasDelLaberinto/HistoriasDelLaberinto/iOS/base/BaseModule.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+protocol Module {
+    var storyboardName: String { get }
+    var controllerName: String { get }
+    var viewController: ViewControllerDisplay { get }
+    var interactor: BusinessLogic { get }
+    var presenter: Presenter { get }
+    var router: RouterLogic { get }
+    
+    init(navigatorProvider: NavigatorProvider)
+}
