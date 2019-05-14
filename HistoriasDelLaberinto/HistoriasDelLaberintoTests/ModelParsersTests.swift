281dfed929af96547fc1a0077f54a20a3adbc730
Francisco del Real Escudero <francisco.real@ciberexperis.es>
2019-05-14 12:09:03 +0200

    Added unitary test targets.

---
+++ HistoriasDelLaberinto/HistoriasDelLaberintoTests/ModelParsersTests.swift
@@ -0,0 +1,98 @@
+//
+//  ModelParsersTests.swift
+//  HistoriasDelLaberintoTests
+//
+//  Created by SYS_CIBER_ADMIN on 11/04/2019.
+//  Copyright © 2019 SetonciOS. All rights reserved.
+//
+
+import XCTest
+@testable import HistoriasDelLaberinto
+
+class ModelParsersTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    func testParseProtagonistModel() {
+        guard let path = Bundle.main.path(forResource: "prota", ofType: "yml", inDirectory: "ExampleFiles") else {
+            XCTFail("Couldn't find prota.yml in loadedGame directory.")
+            return
+        }
+        
+        guard let fileContent = try? String(contentsOfFile: path) else {
+            XCTFail("Unable to read the content of the file in path \(path).")
+            return
+        }
+        
+        let parser = ProtagonistParser()
+        let protagonist = parser.serialize(fileContent)
+        XCTAssertNotNil(protagonist)
+    }
+    
+    func testCharactersProtagonistModel() {
+        guard let path = Bundle.main.path(forResource: "prota", ofType: "yml", inDirectory: "ExampleFiles") else {
+            XCTFail("Couldn't find prota.yml in loadedGame directory.")
+            return
+        }
+        
+        guard let fileContent = try? String(contentsOfFile: path) else {
+            XCTFail("Unable to read the content of the file in path \(path).")
+            return
+        }
+        
+        let parser = ProtagonistParser()
+        let protagonist = parser.serialize(fileContent)
+        XCTAssertNotNil(protagonist)
+    }
+    
+    func testItemsProtagonistModel() {
+        guard let path = Bundle.main.path(forResource: "prota", ofType: "yml", inDirectory: "ExampleFiles") else {
+            XCTFail("Couldn't find prota.yml in loadedGame directory.")
+            return
+        }
+        
+        guard let fileContent = try? String(contentsOfFile: path) else {
+            XCTFail("Unable to read the content of the file in path \(path).")
+            return
+        }
+        
+        let parser = ProtagonistParser()
+        let protagonist = parser.serialize(fileContent)
+        XCTAssertNotNil(protagonist)
+    }
+    
+    func testEventsProtagonistModel() {
+        guard let path = Bundle.main.path(forResource: "prota", ofType: "yml", inDirectory: "ExampleFiles") else {
+            XCTFail("Couldn't find prota.yml in loadedGame directory.")
+            return
+        }
+        
+        guard let fileContent = try? String(contentsOfFile: path) else {
+            XCTFail("Unable to read the content of the file in path \(path).")
+            return
+        }
+        
+        let parser = ProtagonistParser()
+        let protagonist = parser.serialize(fileContent)
+        XCTAssertNotNil(protagonist)
+    }
+    
+    func testRoomsProtagonistModel() {
+        guard let path = Bundle.main.path(forResource: "rooms", ofType: "yml", inDirectory: "ExampleFiles") else {
+            XCTFail("Couldn't find prota.yml in loadedGame directory.")
+            return
+        }
+        
+        guard let fileContent = try? String(contentsOfFile: path) else {
+            XCTFail("Unable to read the content of the file in path \(path).")
+            return
+        }
+        
+        let parser = RoomsFileParser()
+        let rooms = parser.serialize(fileContent)
+        XCTAssertNotNil(rooms)
+    }
+    
+}
