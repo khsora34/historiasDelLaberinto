51c566548bc673fcdc6dc5b9f30ecf89c75ded95
Francisco del Real Escudero <francisco.real@ciberexperis.es>
2019-05-14 18:02:21 +0200

    Removed unnecessary extra file. Fixed database access protocol for it be less generic.

--- HistoriasDelLaberinto/HistoriasDelLaberinto/domain/dao/AllEventDao.swift
+++ HistoriasDelLaberinto/HistoriasDelLaberinto/domain/dao/AllEventDao.swift
@@ -1,7 +1,12 @@
 import UIKit
 import CoreData
 
-class AllEventDao: EventDBAccess {
+protocol AllEventDao {
+    func get(id: String) -> EventDAO?
+    func save(event: EventDAO) -> Bool
+}
+
+class AllEventDaoImpl: AllEventDao {
     func get(id: String) -> EventDAO? {
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return nil }
         let managedContext = appDelegate.persistentContainer.viewContext
@@ -13,11 +18,11 @@ class AllEventDao: EventDBAccess {
         return results?.first
     }
     
-    func save(event: EventDAO) {
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+    func save(event: EventDAO) -> Bool {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return false }
         let managedContext = appDelegate.persistentContainer.viewContext
         
-        guard let entity = NSEntityDescription.entity(forEntityName: "EventDAO", in: managedContext) else { return }
+        guard let entity = NSEntityDescription.entity(forEntityName: "EventDAO", in: managedContext) else { return false }
         let loadingEvent = NSManagedObject(entity: entity, insertInto: managedContext)
         
         loadingEvent.setValue(event.type, forKey: "type")
@@ -25,8 +30,10 @@ class AllEventDao: EventDBAccess {
         
         do {
             try managedContext.save()
+            return true
         } catch let error as NSError {
             print("No ha sido posible guardar \(error), \(error.userInfo)")
+            return false
         }
     }
 }
--- HistoriasDelLaberinto/HistoriasDelLaberinto/domain/dao/EventDbAccess.swift
+++
@@ -1,7 +0,0 @@
-import CoreData.NSManagedObject
-
-protocol EventDBAccess {
-    associatedtype T: NSManagedObject
-    func get(id: String) -> T?
-    func save(event: T)
-}
