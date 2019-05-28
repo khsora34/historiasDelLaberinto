b99cb31f05b42dbda4ea98b6fab4a1336c71472c
Francisco del Real Escudero <francisco.real@ciberexperis.es>
2019-05-28 19:55:36 +0200

    Added rooms fetcher.

---
+++ HistoriasDelLaberinto/HistoriasDelLaberinto/domain/dao/RoomsFetcher.swift
@@ -0,0 +1,94 @@
+import UIKit.UIApplication
+import CoreData
+
+protocol RoomsFetcher {
+    func getRoom(with id: String) -> Room?
+    func saveRoom(for room: Room, with id: String) -> Bool
+}
+
+extension RoomsFetcher {
+    func getRoom(with id: String) -> Room? {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return nil }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest: NSFetchRequest<RoomDAO> = RoomDAO.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "id == %@", id)
+        
+        var room: RoomDAO?
+        do {
+            let results = try managedContext.fetch(fetchRequest)
+            room = results.first
+        } catch let error as NSError {
+            print("No ha sido posible guardar \(error), \(error.userInfo)")
+        }
+        
+        guard let imageUrl = room?.imageUrl, let name = room?.name, let description = room?.descriptionString, let reloadWithPartner = room?.reloadWhenPartner, let isGeneric = room?.isGenericRoom, let actionsSet = room?.actions else { return nil }
+        
+        var actions: [Action] = []
+        
+        for action in actionsSet {
+            if let actionManaged = action as? ActionDAO, let name = actionManaged.name {
+                var condition: Condition?
+                
+                if let type = actionManaged.conditionType, let value = actionManaged.conditionValue {
+                    switch type {
+                    case "item":
+                        condition = .item(id: value)
+                    case "partner":
+                        condition = .partner(id: value)
+                    default:
+                        condition = nil
+                    }
+                }
+                actions.append(Action(name: name, nextStep: actionManaged.nextStep, condition: condition))
+            }
+        }
+        
+        return Room(name: name, description: description, imageUrl: imageUrl, reloadWithPartner: reloadWithPartner, isGenericRoom: isGeneric, actions: actions)
+    }
+    
+    func saveRoom(for room: Room, with id: String) -> Bool {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return false }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        guard let roomEntity = NSEntityDescription.entity(forEntityName: "RoomDAO", in: managedContext),
+            let actionEntity = NSEntityDescription.entity(forEntityName: "ActionDAO", in: managedContext) else { return false }
+        let loadingRoom = NSManagedObject(entity: roomEntity, insertInto: managedContext)
+        
+        loadingRoom.setValue(id, forKey: "id")
+        loadingRoom.setValue(room.name, forKey: "name")
+        loadingRoom.setValue(room.description, forKey: "descriptionString")
+        loadingRoom.setValue(room.imageUrl, forKey: "imageUrl")
+        loadingRoom.setValue(room.isGenericRoom ?? false, forKey: "isGenericRoom")
+        loadingRoom.setValue(room.reloadWithPartner, forKey: "reloadWithPartner")
+        
+        var managedActions: [NSManagedObject] = []
+        
+        for action in room.actions {
+            let loadingAction = NSManagedObject(entity: actionEntity, insertInto: managedContext)
+            loadingAction.setValue(action.name, forKey: "name")
+            loadingAction.setValue(action.nextStep, forKey: "nextStep")
+            if let condition = action.condition {
+                switch condition {
+                case .item(id: let next):
+                    loadingAction.setValue("item", forKey: "conditionType")
+                    loadingAction.setValue(next, forKey: "conditionValue")
+                case .partner(id: let next):
+                    loadingAction.setValue("partner", forKey: "conditionType")
+                    loadingAction.setValue(next, forKey: "conditionValue")
+                }
+            }
+            managedActions.append(loadingAction)
+        }
+        
+        loadingRoom.setValue(NSOrderedSet(array: managedActions), forKey: "actions")
+        
+        do {
+            try managedContext.save()
+            return true
+        } catch let error as NSError {
+            print("No ha sido posible guardar \(error), \(error.userInfo)")
+            return false
+        }
+    }
+}
