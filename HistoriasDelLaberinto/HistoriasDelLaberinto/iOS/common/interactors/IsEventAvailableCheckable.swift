f266e160456c54746c722926a92293ba790f689a
Francisco del Real Escudero <soraquico@gmail.com>
2019-05-31 23:49:35 +0200

    Added is event available interactor protocol.

--- HistoriasDelLaberinto/HistoriasDelLaberinto/iOS/common/interactors/IsEventAvailableCheckable.swift
+++ HistoriasDelLaberinto/HistoriasDelLaberinto/iOS/common/interactors/IsEventAvailableCheckable.swift
@@ -1,9 +1,9 @@
 protocol IsEventAvailableCheckable {
-    func checkEventAvailable(with id: String, eventFetcher: EventsFetcherManager) -> Bool
+    func checkEventAvailable(with id: String, eventFetcher: EventFetcherManager) -> Bool
 }
 
 extension IsEventAvailableCheckable {
-    func checkEventAvailable(with id: String, eventFetcher: EventsFetcherManager) -> Bool {
+    func checkEventAvailable(with id: String, eventFetcher: EventFetcherManager) -> Bool {
         return eventFetcher.getEvent(with: id) != nil
     }
 }
