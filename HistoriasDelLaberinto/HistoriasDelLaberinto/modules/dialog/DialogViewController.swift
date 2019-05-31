80200b06e8f1fa4dd4fdb0daa1a771281155a7c9
Francisco del Real Escudero <soraquico@gmail.com>
2019-05-31 23:52:05 +0200

    Added event handler to dialog.

--- HistoriasDelLaberinto/HistoriasDelLaberinto/modules/dialog/DialogViewController.swift
+++ HistoriasDelLaberinto/HistoriasDelLaberinto/modules/dialog/DialogViewController.swift
@@ -2,22 +2,22 @@ import UIKit
 import Kingfisher
 
 class Dialog {
-    static func createDialog() -> BaseViewController {
-        return DialogViewController()
+    static func createDialog(with configurator: DialogConfigurator, delegate: EventHandler) -> DialogDisplayLogic {
+        let dialog = DialogViewController(configurator)
+        dialog.delegate = delegate
+        dialog.initView()
+        return dialog
     }
 }
 
-protocol DialogDisplayLogic: ViewControllerDisplay {
-    func setFirstConfigurator(configurator: DialogConfigurator)
+protocol DialogDisplayLogic: UIViewController {
+    func setNextConfigurator(newConfigurator: DialogConfigurator)
 }
 
-class DialogViewController: BaseViewController {
+class DialogViewController: UIViewController {
+    private let typingTimeInterval: TimeInterval = 0.02
     
-    private var configurator: DialogConfigurator!
-    
-    var presenter: DialogPresentationLogic? {
-        return _presenter as? DialogPresentationLogic
-    }
+    private var configurator: DialogConfigurator
     
     @IBOutlet weak var dialogView: UIView!
     @IBOutlet weak var characterLabel: UILabel!
@@ -25,7 +25,10 @@ class DialogViewController: BaseViewController {
     @IBOutlet weak var characterImageView: UIImageView!
     @IBOutlet var tapWindowGesture: UITapGestureRecognizer!
     
-    fileprivate init() {
+    weak var delegate: EventHandler?
+    
+    fileprivate init(_ configurator: DialogConfigurator) {
+        self.configurator = configurator
         super.init(nibName: "DialogView", bundle: nil)
         modalPresentationStyle = .overCurrentContext
         modalTransitionStyle = .crossDissolve
@@ -40,9 +43,15 @@ class DialogViewController: BaseViewController {
         dialogView.layer.cornerRadius = 6.0
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setupConfiguration()
+        textView.setTypingText(message: configurator.message, timeInterval: typingTimeInterval)
+    }
+    
     private func setupConfiguration() {
         characterLabel.text = configurator.name
-        textView.text = configurator.message
+        textView.text = ""
         let url = URL(string: configurator.imageUrl)
         characterImageView.kf.setImage(with: url, placeholder: nil, options: [.transition(.fade(0.1))]) { [weak self] (result) in
             switch result {
@@ -56,11 +65,12 @@ class DialogViewController: BaseViewController {
     }
     
     @IBAction func didTouchView(_ sender: Any) {
-        guard let newConfigurator = presenter?.getNextStep().dialogConfigurator else {
-            self.dismiss(animated: true, completion: nil)
-            return
-        }
-        
+        delegate?.continueFlow()
+    }
+}
+
+extension DialogViewController: DialogDisplayLogic {
+    func setNextConfigurator(newConfigurator: DialogConfigurator) {
         if newConfigurator.imageUrl != configurator.imageUrl {
             UIView.animate(withDuration: 0.3, delay: 0.0, options: UIView.AnimationOptions.transitionCrossDissolve, animations: {
                 self.characterImageView.alpha = 0.3
@@ -72,21 +82,15 @@ class DialogViewController: BaseViewController {
                 UIView.animate(withDuration: 0.2, delay: 0.0, options: UIView.AnimationOptions.curveEaseIn, animations: {
                     self.characterImageView.alpha = 1.0
                     self.dialogView.alpha = 0.95
-                }, completion: nil)
+                }, completion: { _ in
+                    self.textView.setTypingText(message: self.configurator.message, timeInterval: self.typingTimeInterval)
+                })
             })
             
-//            characterImageView.image = nil
         } else {
             configurator = newConfigurator
             setupConfiguration()
-
+            textView.setTypingText(message: configurator.message, timeInterval: typingTimeInterval)
         }
-    }
-}
-
-extension DialogViewController: DialogDisplayLogic {
-    func setFirstConfigurator(configurator: DialogConfigurator) {
-        self.configurator = configurator
-        setupConfiguration()
     }
 }
