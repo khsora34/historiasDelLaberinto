37bc4c5646e3cede6a78c236a34cce7e1c714110
Francisco del Real Escudero <soraquico@gmail.com>
2019-06-01 20:29:54 +0200

    Addedd colors extension.

---
+++ HistoriasDelLaberinto/HistoriasDelLaberinto/iOS/extensions/UIColorExtension.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+extension UIColor {
+    static var coolBlue: UIColor {
+        return UIColor(hue: 211/360, saturation: 1, brightness: 1, alpha: 1)
+    }
+    
+    static var darkCoolBlue: UIColor {
+        return UIColor(hue: 211/360, saturation: 1, brightness: 0.5, alpha: 1)
+    }
+}
