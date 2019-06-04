ccfd0c9b778edaa02777efce6ce896207cc68f1d
Francisco del Real Escudero <soraquico@gmail.com>
2019-06-04 22:45:04 +0200

    Added example for trying movement.

--- HistoriasDelLaberinto/HistoriasDelLaberinto/modules/exampleScene/ExampleSceneRouter.swift
+++ HistoriasDelLaberinto/HistoriasDelLaberinto/modules/exampleScene/ExampleSceneRouter.swift
@@ -16,9 +16,14 @@ class ExampleSceneRouter: BaseRouter, ExampleSceneRoutingLogic {
     }
     
     func goToNewView() {
-        guard let navigator = drawer?.currentRootViewController as? UINavigationController else { return }
-        let module = moduleProvider.exampleSceneModule()
-        navigator.setViewControllers([module.viewController], animated: true)
+        let controller = moduleProvider.movementSceneModule().viewController
+        controller.providesPresentationContextTransitionStyle = true
+        controller.definesPresentationContext = true
+        controller.modalPresentationStyle = .overCurrentContext
+        addBlur()
+        drawer?.present(controller, animated: true, completion: nil)
+    }
+    
     private func addBlur() {
         guard let drawer = drawer else { return }
         let blurView = UIVisualEffectView()
