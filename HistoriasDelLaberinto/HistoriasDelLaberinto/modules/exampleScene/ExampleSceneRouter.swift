8092e6c2f955a5d0027866b17e2fd68c49a88584
Francisco del Real Escudero <soraquico@gmail.com>
2019-06-04 22:44:54 +0200

    Added func to add blur to drawer.

--- HistoriasDelLaberinto/HistoriasDelLaberinto/modules/exampleScene/ExampleSceneRouter.swift
+++ HistoriasDelLaberinto/HistoriasDelLaberinto/modules/exampleScene/ExampleSceneRouter.swift
@@ -19,6 +19,12 @@ class ExampleSceneRouter: BaseRouter, ExampleSceneRoutingLogic {
         guard let navigator = drawer?.currentRootViewController as? UINavigationController else { return }
         let module = moduleProvider.exampleSceneModule()
         navigator.setViewControllers([module.viewController], animated: true)
+    private func addBlur() {
+        guard let drawer = drawer else { return }
+        let blurView = UIVisualEffectView()
+        blurView.frame = drawer.view.frame
+        blurView.effect = UIBlurEffect(style: .dark)
+        drawer.view.addSubview(blurView)
     }
     
     func present(_ controller: UIViewController, animated: Bool) {
