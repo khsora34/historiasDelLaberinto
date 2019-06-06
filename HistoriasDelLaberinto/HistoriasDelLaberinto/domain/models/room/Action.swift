bac7a75aede9fc9e3c5a1da4b86ba2737327c54d
Francisco del Real Escudero <soraquico@gmail.com>
2019-06-06 23:45:07 +0200

    Added equatable to actions.

--- HistoriasDelLaberinto/HistoriasDelLaberinto/domain/models/room/Action.swift
+++ HistoriasDelLaberinto/HistoriasDelLaberinto/domain/models/room/Action.swift
@@ -10,4 +10,24 @@ struct Action: Codable {
     }
 }
 
+extension Action: Equatable {
+    static func == (lhs: Action, rhs: Action) -> Bool {
+        guard lhs.name == rhs.name, lhs.nextStep == rhs.nextStep else { return false }
+        switch (lhs.condition, rhs.condition) {
+        case (.item(let id1)?, .item(let id2)?):
+            return id1 == id2
+        case (.roomVisited(let id1)?, .roomVisited(let id2)?):
+            return id1 == id2
+        case (.roomNotVisited(let id1)?, .roomNotVisited(let id2)?):
+            return id1 == id2
+        case (.partner(let id1)?, .partner(let id2)?):
+            return id1 == id2
+        case (.none, .none):
+            return true
+        default:
+            return false
+        }
+    }
+}
+
 typealias ActionParser = YamlParser<Action>
