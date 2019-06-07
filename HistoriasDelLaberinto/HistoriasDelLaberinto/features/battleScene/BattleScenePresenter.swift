3e172aa0ca4a1d11569f68605a1090b0f72ec24a
Francisco del Real Escudero <soraquico@gmail.com>
2019-06-08 00:15:22 +0200

    Using new variables for drawing views.

--- HistoriasDelLaberinto/HistoriasDelLaberinto/features/battleScene/BattleScenePresenter.swift
+++ HistoriasDelLaberinto/HistoriasDelLaberinto/features/battleScene/BattleScenePresenter.swift
@@ -23,11 +23,14 @@ class BattleScenePresenter: BasePresenter {
     init(enemy: PlayableCharacter) {
         self.enemy = enemy
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        viewController?.addCharactersStatus([])
+        viewController?.setBackground(with: delegate?.imageUrl)
         getProtagonist()
         getPartner()
+        buildCharacters()
+        buildEnemy()
     }
 }
 
@@ -45,5 +48,20 @@ extension BattleScenePresenter {
         let request = BattleScene.CharacterGetter.Request(id: partnerId)
         let response = interactor?.getPartner(request: request)
         partner = response?.character
+    }
+    
+    private func buildCharacters() {
+        let protagonistModel = StatusViewModel(name: protagonist.name, ailment: protagonist.currentStatusAilment, actualHealth: protagonist.currentHealthPoints, maxHealth: protagonist.maxHealthPoints, imageUrl: protagonist.imageUrl, isEnemy: false, didTouchView: nil)
+        var charactersForStatus: [StatusViewModel] = [protagonistModel]
+        if let partner = partner {
+            let partnerModel = StatusViewModel(name: partner.name, ailment: partner.currentStatusAilment, actualHealth: partner.currentHealthPoints, maxHealth: partner.maxHealthPoints, imageUrl: partner.imageUrl, isEnemy: false, didTouchView: nil)
+            charactersForStatus.append(partnerModel)
+        }
+        viewController?.addCharactersStatus(charactersForStatus)
+    }
+    
+    private func buildEnemy() {
+        let model = StatusViewModel(name: enemy.name, ailment: enemy.currentStatusAilment, actualHealth: enemy.currentHealthPoints, maxHealth: enemy.maxHealthPoints, imageUrl: "https://cdn4.iconfinder.com/data/icons/eldorado-culture/40/ghost-512.png", isEnemy: true, didTouchView: nil)
+        viewController?.setEnemyInfo(imageUrl: enemy.imageUrl, model: model)
     }
 }
