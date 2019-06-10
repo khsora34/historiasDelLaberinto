e6de182c50e9d42c92e1437b3a87574456eed141
Francisco del Real Escudero <soraquico@gmail.com>
2019-06-10 18:44:30 +0200

    Refactored dialog launch on external protocol.

---
+++ HistoriasDelLaberinto/HistoriasDelLaberinto/iOS/common/DialogLauncher.swift
@@ -0,0 +1,20 @@
+protocol DialogLauncher: NextDialogHandler {
+    var dialog: DialogDisplayLogic? { get set }
+    var viewController: BattleSceneDisplayLogic? { get }
+    func present(_ dialog: DialogDisplayLogic)
+}
+
+extension DialogLauncher {
+    func showDialog(with configurator: DialogConfigurator) {
+        guard dialog == nil else {
+            dialog?.setNextConfigurator(configurator)
+            if viewController?.presentedViewController == nil {
+                present(dialog!)
+            }
+            return
+        }
+        dialog = Dialog.createDialog(configurator, delegate: self)
+        present(dialog!)
+    }
+    
+}
