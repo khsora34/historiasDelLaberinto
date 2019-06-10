c59fad166adb0e37a630e09a7e6100a5f7c167b4
Francisco del Real Escudero <soraquico@gmail.com>
2019-06-10 19:24:50 +0200

    Added shake func.

---
+++ HistoriasDelLaberinto/HistoriasDelLaberinto/iOS/extensions/UIViewExtension.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewExtension.swift
+//  HistoriasDelLaberinto
+//
+//  Created by SYS_CIBER_ADMIN on 10/06/2019.
+//  Copyright © 2019 SetonciOS. All rights reserved.
+//
+
+import Foundation
