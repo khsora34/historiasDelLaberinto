1f31e3a13a1fb8fe9fe84354aa917c09706d2e2c
Francisco del Real Escudero <soraquico@gmail.com>
2019-09-12 21:37:26 +0200

    Removed unnecessary functionality.

--- HistoriasDelLaberinto/HistoriasDelLaberinto/domain/dao/events/EventTypeFetcher.swift
+++ HistoriasDelLaberinto/HistoriasDelLaberinto/domain/dao/events/EventTypeFetcher.swift
@@ -3,7 +3,6 @@ import CoreData
 
 protocol EventTypeFetcher {
     func getEventType(with id: String) -> EventDAO?
-    func saveEventType(for event: Event, with id: String) -> Bool
     func deleteAllEventTypes()
 }
 
@@ -17,27 +16,6 @@ extension EventTypeFetcher {
         
         let results = try? managedContext.fetch(fetchRequest)
         return results?.first
-    }
-    
-    func saveEventType(for event: Event, with id: String) -> Bool {
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return false }
-        let managedContext = appDelegate.persistentContainer.viewContext
-        
-        guard let entity = NSEntityDescription.entity(forEntityName: "EventDAO", in: managedContext) else { return false }
-        let loadingEvent = NSManagedObject(entity: entity, insertInto: managedContext)
-        
-        guard let eventType = EventType(event: event) else { return false }
-        
-        loadingEvent.setValue(eventType.rawValue, forKey: "type")
-        loadingEvent.setValue(id, forKey: "id")
-        
-        do {
-            try managedContext.save()
-            return true
-        } catch let error as NSError {
-            print("No ha sido posible guardar \(error), \(error.userInfo)")
-            return false
-        }
     }
     
     func deleteAllEventTypes() {
