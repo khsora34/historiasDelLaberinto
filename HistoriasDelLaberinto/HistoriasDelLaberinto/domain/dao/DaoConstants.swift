f40c70076992d83c96413a3cc466d09a9de7b88c
Francisco del Real Escudero <soraquico@gmail.com>
2019-09-30 17:00:45 +0200

    NEW: Added constants for database attributes names. (#25942496)

---
+++ HistoriasDelLaberinto/HistoriasDelLaberinto/domain/dao/DaoConstants.swift
@@ -0,0 +1,53 @@
+enum DaoConstants {
+    enum Character: String {
+        case id, imageUrl, name, agility, attack, currentHealthPoints, defense, maxHealthPoints, portraitUrl, weaponId, partner, inventory
+    }
+    
+    enum ModelsNames: String {
+        case ActionDAO, BattleEventDAO, CharacterDAO, ChoiceEventDAO, ConditionEventDAO, ConsumableItemDAO, DialogueEventDAO, EventDAO, ItemDAO, Movement, ItemsQuantity, PlayableCharacterDAO, ProtagonistDAO, RewardEventDAO, RoomDAO, RoomPosition, WeaponDAO
+    }
+    
+    enum Action: String {
+        case conditionType, conditionValue, name, nextStep
+    }
+    
+    enum BattleEvent: String {
+        case enemyId, loseStep, winStep
+    }
+    
+    enum ChoiceEvent: String {
+        case actionsAssociated
+    }
+    
+    enum ConditionEvent: String {
+        case conditionType, conditionValue, nextStepIfFalse, nextStepIfTrue
+    }
+    
+    enum DialogueEvent: String {
+        case characterId, message, nextStep
+    }
+    
+    enum Event: String {
+        case id, shouldEndGame, shouldSetVisited, type
+    }
+    
+    enum Item: String {
+        case healthRecovered, descriptionString, id, imageUrl, type, name, itemId, quantity, ailment, extraDamage, hitRate, induceRate
+    }
+    
+    enum Movement: String {
+        case actualX, actualY, compassPoint, genericProb
+    }
+    
+    enum RewardEvent: String {
+        case message, nextStep, rewardsAssociated
+    }
+    
+    enum Room: String {
+        case id, descriptionString, imageUrl, isGenericRoom, isVisited, isVisitedWithPartner, name, actions
+    }
+    
+    enum RoomPosition: String {
+        case roomId, x, y
+    }
+}
