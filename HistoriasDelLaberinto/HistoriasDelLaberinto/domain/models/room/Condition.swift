3ec107e5beea869f0d3a36d6ab7fcd64fb5291e3
Francisco del Real Escudero <soraquico@gmail.com>
2019-10-03 11:20:45 +0200

    NEW: Added ConditionString enum for using it as constants.  (#25942496)

--- HistoriasDelLaberinto/HistoriasDelLaberinto/domain/models/room/Condition.swift
+++ HistoriasDelLaberinto/HistoriasDelLaberinto/domain/models/room/Condition.swift
@@ -22,23 +22,28 @@ extension Condition: Decodable {
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         let rawValue = try container.decode(String.self, forKey: .rawValue)
-        switch rawValue {
-        case "partner":
+        let parsedValue = ConditionString(rawValue: rawValue)
+        switch parsedValue {
+        case .partner:
             let partnerId = try container.decode(String.self, forKey: .associatedValue)
             self = .partner(id: partnerId)
-        case "item":
+        case .item:
             let itemId = try container.decode(String.self, forKey: .associatedValue)
             self = .item(id: itemId)
-        case "roomVisited":
+        case .roomVisited:
             let roomId = try container.decode(String.self, forKey: .associatedValue)
             self = .roomVisited(id: roomId)
-        case "roomNotVisited":
+        case .roomNotVisited:
             let roomId = try container.decode(String.self, forKey: .associatedValue)
             self = .roomNotVisited(id: roomId)
         default:
             throw CodingError.unknownValue
         }
     }
+}
+
+enum ConditionString: String {
+    case partner, item, roomVisited, roomNotVisited
 }
 
 typealias ConditionParser = YamlParser<Condition>
